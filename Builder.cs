d30d2b11d5bd3613c62ed80e109f6ee4b2111e47
Pyro569 <79991107+Pyro569@users.noreply.github.com>
2023-10-01 19:55:07 -0700

    Better Documentation

--- Builder.cs
+++ Builder.cs
@@ -7,33 +7,35 @@ namespace DawnLangCompiler
 {
     class Builder
     {
-        private static List<string> Lines = new List<string>();
-        private static List<string> Tokens = new List<string>();
-        private static List<string> ConvertedTokens = new List<string>();
-        private static List<string> IntVars = new List<string>();
-        private static List<string> StringVars = new List<string>();
-        private static string ErrorOpCode = "a000";
+        private static List<string> Lines = new List<string>();     //lines in the original dawnlang file
+        private static List<string> Tokens = new List<string>();    //tokens from the original dawnlang file
+        private static List<string> ConvertedTokens = new List<string>();   //the C version of the tokens from the dawnlang file
+        private static List<string> IntVars = new List<string>();           //names of integer variables stored in the program
+        private static List<string> StringVars = new List<string>();        //names of string variables stored in the program
+        private static List<string> RequiredImports = new List<string>();   //imports required to make the C code work
+        private static string ErrorOpCode = "a000";                         //random junk output for errors that actually has a meaning once you look at the source code
 
         public static void BuildFile(string FilePath, string OutputFileName)
         {
             try
             {
-                ReadFile(FilePath);
-                ConvertTokens();
-                CreateCFile();
-                CompileCFile(OutputFileName);
-                Cleanup();
+                ReadFile(FilePath);     //get the tokens of the fike
+                CheckForImports();      //check for required imports to make it work in C
+                ConvertTokens();        //convert the code to C
+                CreateCFile();          //write the C code into a file
+                CompileCFile(OutputFileName);   //compile the C file hopefully
+                Cleanup();              //cleanup all of the leftovers
             }
             catch
-            {
+            {   //print out the error code and do some cleanup if there is an error
                 Console.WriteLine("ERROR CODE: " + ErrorOpCode);
+                Cleanup();
+                if (File.Exists(OutputFileName))    //remove the probably fucked binary if it exists and compiled
+                    File.Delete(OutputFileName);
             }
         }
         public static void ReadFile(string FilePath)
         {
-            Lines.Clear();
-            Tokens.Clear();
-
             ErrorOpCode = "r100";    //r for read, 100 for first potential error spot
 
             //read the file's lines and add to the lines list
@@ -52,6 +54,7 @@ namespace DawnLangCompiler
             //run through each line and parse tokens then add to the tokens list
             for (int i = 0; i < Lines.Count; i++)
             {
+                //add semicolon to end to allow for easier breaking of lines in parsing
                 if (!Lines[i].EndsWith(";"))
                 {
                     Lines[i] += ";";
@@ -64,24 +67,27 @@ namespace DawnLangCompiler
                 {//loop through length of string in Lines[i]
 
                     //if quotation marks, mark the beginning of quotation for tokens to be 1 token
-                    if (Lines[i][j] == '"' && Quotation == false)
+                    if (Lines[i][j] == '"' && Quotation == false)   //begin quotation
                     {
                         Quotation = true;
                     }
-                    else if (Lines[i][j] == '"' && Quotation == true)
+                    else if (Lines[i][j] == '"' && Quotation == true)   //end quotation
                     {
                         Quotation = false;
                     }
 
+                    //add space to string if in quote and space
                     if (Lines[i][j] == ' ' && Quotation == true)
                     {
                         TokenString += " ";
                     }
+                    //if not semicolon space or parenthesis then add current char to string
                     else if (Lines[i][j] != ';' && Lines[i][j] != ' ' && Lines[i][j] != '(' && Lines[i][j] != ')')
                     {
                         TokenString += Lines[i][j];
                     }
                     else
+                    //add the string to the tokens and set string to blank
                     {
                         Tokens.Add(TokenString);
                         TokenString = "";
@@ -94,12 +100,31 @@ namespace DawnLangCompiler
             //    System.Console.WriteLine(token);
         }
 
+        private static void CheckForImports()
+        {
+            //list of dawnlang functions that will require some kind of import in C code
+            List<string> FunctionsRequiringImports = new List<string>(){
+                "print",
+                "print_int",
+                "print_str",
+            };
+
+            //the needed import for said dawnlang function
+            List<string> CorrespondingImport = new List<string>(){
+                "<stdio.h>",    //print
+                "<stdio.h>",    //print_int
+                "<stdio.h>",    //print_str
+            };
+
+            for (int i = 0; i < FunctionsRequiringImports.Count; i++)   //loop through each function in the FunctionsRequiringImports list
+                if (Tokens.Contains(FunctionsRequiringImports[i]) && !RequiredImports.Contains(CorrespondingImport[i]))
+                    //if the tokens list contains said function and the requiredimports list does not already have it, add it
+                    RequiredImports.Add(CorrespondingImport[i]);
+        }
+
         private static void ConvertTokens()
         {
-            ConvertedTokens.Clear();
-
             //convert tokens to c code
-
             ErrorOpCode = "c100";       //c for conversion, 100 for first potential error spot
 
             for (int i = 0; i < Tokens.Count; i++)
@@ -107,30 +132,31 @@ namespace DawnLangCompiler
                 switch (Tokens[i])
                 {
                     case "print":
-                        ConvertedTokens.Add("printf(" + Tokens[i + 1] + ");");
+                        ConvertedTokens.Add("printf(" + Tokens[i + 1] + ");");              //print("hello, world"); comes out to printf("hello, world");
                         break;
                     case "print_str":
-                        ConvertedTokens.Add("printf(\"%s\\n\"," + Tokens[i + 1] + ");");
+                        ConvertedTokens.Add("printf(\"%s\\n\"," + Tokens[i + 1] + ");");    //print_str(hello); comes out to printf("%s", hello);
                         break;
                     case "int":
-                        ConvertedTokens.Add("int " + Tokens[i + 1] + Tokens[i + 2] + Tokens[i + 3] + ";");
+                        ConvertedTokens.Add("int " + Tokens[i + 1] + Tokens[i + 2] + Tokens[i + 3] + ";");  //int a = 17;
                         IntVars.Add(Tokens[i + 1]);
                         break;
                     case "string":
-                        ConvertedTokens.Add("char " + Tokens[i + 1] + "[]" + Tokens[i + 2] + Tokens[i + 3] + ";");
+                        ConvertedTokens.Add("char " + Tokens[i + 1] + "[]" + Tokens[i + 2] + Tokens[i + 3] + ";");  //string hello = "hello world!"; comes out to char[] hello = "hello world!';
                         StringVars.Add(Tokens[i + 1]);
                         break;
                     case "print_int":
-                        ConvertedTokens.Add("printf(\"%d\\n\"," + Tokens[i + 1] + ");");
+                        ConvertedTokens.Add("printf(\"%d\\n\"," + Tokens[i + 1] + ");");    //print_int(a); comes out to printf("%d\n",a);
                         break;
                     case "for":
-                        ConvertedTokens.Add("for(int i = " + Tokens[i + 3] + "; i < " + Tokens[i + 5] + "; i++){");
+                        ConvertedTokens.Add("for(int i = " + Tokens[i + 3] + "; i <= " + Tokens[i + 5] + "; i++){");    //create a for loop
                         break;
                     case "then":
-                        if (Tokens[i + 1] == "end")
+                        if (Tokens[i + 1] == "end")         //end a for loop with a };
                             ConvertedTokens.Add("};");
                         break;
                     default:
+                        //change the value of an int variable
                         if (IntVars.Contains(Tokens[i]))
                         {
                             if (Tokens[i + 1] == "=")
@@ -149,14 +175,23 @@ namespace DawnLangCompiler
 
         private static void CreateCFile()
         {
-            ErrorOpCode = "wc100";       //w for writing c, 100 for first potential error spot
+            ErrorOpCode = "wc100";       //wc for writing c, 100 for first potential error spot
 
             StreamWriter outputFile = new StreamWriter("TempFile.c");
-            outputFile.WriteLine("#include <stdio.h>\nint main() {");
+
+            //add the required imports to the top of the c file
+            for (int i = 0; i < RequiredImports.Count; i++)
+            {
+                outputFile.WriteLine("#include " + RequiredImports[i] + "\n");
+            }
+
+            outputFile.WriteLine("int main() {");   //create a basic C file main function
+            //write each converted token to the c file
             foreach (string codeLines in ConvertedTokens)
             {
                 outputFile.WriteLine(codeLines);
             }
+            //close off the c file with a return 0; and a right brace
             outputFile.WriteLine("return 0;\n}");
             outputFile.Close();
         }
@@ -165,7 +200,7 @@ namespace DawnLangCompiler
         {
             ErrorOpCode = "cf100";              //cf for compile file, 100 for first potential error spot
 
-            if (File.Exists(OutputFileName))
+            if (File.Exists(OutputFileName))    //if the compiled binary already exists with that name, delete it/overwrite it
                 File.Delete(OutputFileName);
 
             ErrorOpCode = "cf200";              //cf for compile file, 200 for second potential error spot
@@ -179,7 +214,7 @@ namespace DawnLangCompiler
         {
             ErrorOpCode = "cl100";              //cl for cleanup, 100 for first potential error spot
 
-            if (File.Exists("./TempFile.c"))
+            if (File.Exists("./TempFile.c"))    //delete the TempFile.c if it still exists (which it still should, if not, error)
             {
                 File.Delete("./TempFile.c");
             }
--- testfile.dwl
+++ testfile.dwl
@@ -4,4 +4,3 @@ string hello = "hello world";
 print_str(hello);
 a = 13;
 print_int(a);
-for i = 0 to 100 do print_int(i), print_str(hello) then end; 
